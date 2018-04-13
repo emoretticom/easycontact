115a14559cd0c8f79314951ac64cf7dfd4ff8c9b
ettore moretti <ettoremoretti27@gmail.com>
2018-04-13 15:54:26 +0200

    a

--- src/core/Contact.php
+++ src/core/Contact.php
@@ -1,15 +1,18 @@
 <?php
 namespace emoretti\easycontact;
 
+require_once(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."config.php");
+
+
 class Contact {
 	public function __construct() {
 		echo "<pre>";
 		print_r(CONTACT_DATA);
 		exit;
 	}
-	
-	
+
+
 	public function execute(){
 	}
-	
+
 }
